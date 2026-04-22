1df5c983fa7e4b4a70625434434921d2a350c767
djmisterjon <jon.lepage@live.ca>
2026-04-21 20:29:39 -0400

    feat: update project structure and build paths for improved compatibility with Unity

--- lsde-ts/scripts/publish.sh
+++ lsde-ts/scripts/publish.sh
@@ -20,7 +20,7 @@ ROOT="$(cd "$SCRIPT_DIR/../.." && pwd -W 2>/dev/null || pwd)"
 TS_DIR="$ROOT/lsde-ts"
 CS_DIR="$ROOT/lsde-csharp/Runtime"
 CSPROJ="$CS_DIR/LsdeDialogEngine.csproj"
-CS_STJ_CSPROJ="$ROOT/lsde-csharp/src/LsdeDialogEngine.SystemTextJson/LsdeDialogEngine.SystemTextJson.csproj"
+CS_STJ_CSPROJ="$ROOT/lsde-csharp/src~/LsdeDialogEngine.SystemTextJson/LsdeDialogEngine.SystemTextJson.csproj"
 CS_NJ_CSPROJ="$ROOT/lsde-csharp/Runtime/Newtonsoft/LsdeDialogEngine.Newtonsoft.csproj"
 
 TARGET="both"
